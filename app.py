7331a1f9b5204f76c1d6224695ba9ae74c8ef474
Vaibhav Soni <vaibhavgear7@gmail.com>
2023-02-22 22:43:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,8 +19,8 @@ with st.echo(code_location="below"):
     
     
 with st.echo(code_location="above"):    
-st.write("Here's our first attempt at using data to create a table:")
-st.write(pd.DataFrame({
+    st.write("Here's our first attempt at using data to create a table:")
+    st.write(pd.DataFrame({
     'first column': [1, 2, 3, 4],
     'second column': [10, 20, 30, 40]
 }))
