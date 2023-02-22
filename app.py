fabe3709720e65a4bab5f072a054d537ea4287b2
Vaibhav Soni <vaibhavgear7@gmail.com>
2023-02-23 00:04:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ Here's our first attempt at using data to create a table:
 
 import streamlit as st
 import pandas as pd
+import numpy as np
 
-
-with st.echo(code_location="below"):
+with st.echo(code_location="above"):
     df = pd.DataFrame({
       'first column': [1, 2, 3, 4],
       'second column': [10, 20, 30, 40]
@@ -15,7 +15,6 @@ with st.echo(code_location="below"):
 
     df
 
-    st.table(df)
     
     
 with st.echo(code_location="above"):    
@@ -24,3 +23,15 @@ with st.echo(code_location="above"):
     'first column': [1, 2, 3, 4],
     'second column': [10, 20, 30, 40]
 }))
+
+with st.echo(code_location="above"):    
+    dataframe = np.random.randn(10, 20)
+    st.dataframe(dataframe)
+    
+    
+with st.echo(code_location="above"):  
+    dataframe = pd.DataFrame(
+    np.random.randn(10, 20),
+    columns=('col %d' % i for i in range(20)))
+
+    st.dataframe(dataframe.style.highlight_max(axis=0))
